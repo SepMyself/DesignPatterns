2d1ffc0503d030697f32b441afbfd4d8f6fd1ddb
Allen <hong3.wang@changhong.com>
2018-06-28 17:50:10 +0800

    add some comments

--- src/com/wh/AbstractFactory/Client.java
+++ src/com/wh/AbstractFactory/Client.java
@@ -1,5 +1,8 @@
 package com.wh.AbstractFactory;
 
+/**
+ * 抽象工厂模式则需要面对多个产品等级结构。
+ */
 public class Client {
     public static void main(String[] args){
         ComputerEngineer cf = new ComputerEngineer();
--- src/com/wh/FactoryMethod/Client.java
+++ src/com/wh/FactoryMethod/Client.java
@@ -5,6 +5,9 @@ import com.wh.AbstractFactory.IAbstractFactory;
 import com.wh.AbstractFactory.IntelFactory;
 import com.wh.SimpleFactory.ICpu;
 
+/**
+ * 工厂方法模式针对的是一个产品等级结构
+ */
 public class Client {
     public static void main(String[] args){
         //IAbstractFactory factory = new IntelFactory();
